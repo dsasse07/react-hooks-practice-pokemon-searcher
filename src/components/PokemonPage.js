07cd63a569f9f9c51767f619cf43d77b36546515
Daniel Sasse <dsasse07@gmail.com>
2021-02-27 12:23:37 -0500

    commit tree 68937d59f851cb2db437c551ca3a9e55ebbed8f9

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+
+  const [search, setSearch ] = useState("")
+  const [pokemonData, setPokemonData] = useState([])
+  const API_URL = "http://localhost:3001/pokemon"
+
+  useEffect(() => {
+    
+    fetch(API_URL)
+      .then( response => response.json() )
+      .then( setPokemonData )
+
+  }, [])
+
+  function handleFormSubmit(formData){
+    const postConfig={
+      method:"POST",
+      headers:{
+        "Content-type":"application/json"
+      },
+      body: JSON.stringify( formData )
+    }
+
+    fetch(API_URL, postConfig)
+      .then( response => response.json() )
+      .then( newPokemon => {
+        setPokemonData([...pokemonData, newPokemon])
+      })
+  }
+
+  function handleSearchChange(event){
+    setSearch(event.target.value)
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onFormSubmit={handleFormSubmit}/>
       <br />
-      <Search />
+      <Search search={search} onSearchChange={handleSearchChange}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection search={search} pokemonData={pokemonData}/>
     </Container>
   );
 }
